75c9f1f6e6efa61f002b55f53cb4c535d5c32517
rodolphe <wachter@intechinfo.fr>
2015-11-30 13:44:30 +0100

    Remise à niveau

--- script/chargervar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-address=88.177.168.133
-zonerep=/etc/tinydns/root
-zonefile=zonerep/data
---
+++ script/domainRainloop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+domain=$1
+
+if [ ! -f "/var/www/rainloop/data/_data_/_default_/domains/$domain.ini" ];then
+	sudo touch /var/www/rainloop/data/_data_/_default_/domains/$domain.ini
+	sudo echo 'map_host = "localhost"
+imap_port = 143
+imap_secure = "None"
+imap_short_login = Off
+sieve_use = Off
+sieve_allow_raw = Off
+sieve_host = ""
+sieve_port = 4190
+sieve_secure = "None"
+smtp_host = "10.8.96.3"
+smtp_port = 25
+smtp_secure = "None"
+smtp_short_login = Off
+smtp_auth = Off
+smtp_php_mail = Off
+white_list = ""' >> /var/www/rainloop/data/_data_/_default_/domains/$domain.ini
+fi
+
