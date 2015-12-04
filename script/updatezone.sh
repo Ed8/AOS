daf4db89f25f074af9da9d4ef5da7dbd0c2860fa
DimtriTchapmi <tchapmiwagna@intechinfo.fr>
2015-12-04 05:00:01 -0800

    Modification script dns

--- script/updatezone.sh
+++ script/updatezone.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo cat /etc/tinydns/root/*.zone > /etc/tinydns/root/data
+cd /etc/tinydns/root/
 sudo rm /etc/tinydns/root/data.cdb
-sudo make /etc/tinydns/root/data
+sudo make 
 sudo ssh -i /home/dimitri/.ssh/id_rsa root@dedibox.itinet.fr
