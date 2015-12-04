6a9edfc70315b68a9280f4853ab5323786243ccf
rodolphe <wachter@intechinfo.fr>
2015-12-04 14:51:48 +0100

    maj enregistrement

--- script/addenregzone.sh
+++ script/addenregzone.sh
@@ -5,11 +5,11 @@ enrg=$3
 adresse=$4
 if [ $enrg = "mail" ]; then
 	if [ -e "/etc/tinydns/root/$nomuser.zone" ]; then
-		if sudo grep -q @$nomuser.$domaine /etc/tinydns/root/$nomuser.zone; then
+		if sudo grep -q @$domaine /etc/tinydns/root/$nomuser.zone; then
             		sudo echo "Ce mail existe deja!"
          	else
 			if [ $adresse = "88.177.168.133" ]; then
-				sudo echo "@$nomuser.$domaine:$adresse:mx:86400" >> /etc/tinydns/root/$nomuser.zone	
+				sudo echo "@$domaine:$adresse::86400" >> /etc/tinydns/root/$nomuser.zone	
 				verification=`sudo grep $domaine /etc/postfix/main.cf`
 	   			if [ -z "$verification" ]; then
 	   				sudo sed -i '10 s/$/'\ $domaine'/g' /etc/postfix/main.cf
@@ -18,7 +18,7 @@ if [ $enrg = "mail" ]; then
 					sudo bash domainRainloop.sh $domaine
 				fi
 			else				
-				sudo echo "@$nomuser.$domaine:$adresse:mx:86400" >> /etc/tinydns/root/$nomuser.zone				
+				sudo echo "@$domaine:$adresse::86400" >> /etc/tinydns/root/$nomuser.zone				
            		fi
 					
 	    	fi	   		
@@ -30,15 +30,15 @@ elif [ $enrg = "web" ]; then
 		if sudo grep -q +www.$nomuser.$domaine /etc/tinydns/root/$nomuser.zone; then
 			sudo echo "Ce web existe deja!"
 		else
- 	   		sudo echo "+www.$nomuser.$domaine:$adresse:a:86400 " >> /etc/tinydns/root/$nomuser.zone
-			sudo echo "+www.dev.$nomuser.$domaine:$adresse:a:86400 " >> /etc/tinydns/root/$nomuser.zone
+ 	   		sudo echo "+www.$nomuser.$domaine:$adresse::86400 " >> /etc/tinydns/root/$nomuser.zone
+			sudo echo "+www.dev.$nomuser.$domaine:$adresse::86400 " >> /etc/tinydns/root/$nomuser.zone
 		fi
 
 	elif sudo grep -q "+www.$nomuser" "/etc/tinydns/root/aos.zone"; then
   	   	sudo echo "Le web existe deja!"
 	else
-  	 	sudo echo "+www.$nomuser.$domaine:88.177.168.133:a:86400 " >> /etc/tinydns/root/aos.zone
-		sudo echo "+www.dev.$nomuser.$domaine:88.177.168.133:a:86400 " >> /etc/tinydns/root/aos.zone
+  	 	sudo echo "+www.$nomuser.$domaine:88.177.168.133::86400 " >> /etc/tinydns/root/aos.zone
+		sudo echo "+www.dev.$nomuser.$domaine:88.177.168.133::86400 " >> /etc/tinydns/root/aos.zone
 	fi
 fi
 sudo bash updatezone.sh
--- script/delzone.sh
+++ script/delzone.sh
@@ -14,8 +14,8 @@ if [ $enrg = "domaine" -a $domaine != "aos.itinet.fr" ]; then
 	fi
 elif [ $enrg = "mail" -a "$domaine" != "aos.itinet.fr" ]; then
 	if [ $adresse = "88.177.168.133" ]; then
-		if sudo grep -q  @$nomuser.$domaine:$adresse /etc/tinydns/root/$nomuser.zone; then
-			sudo sed -i /"@$nomuser.$domaine:$adresse"/d /etc/tinydns/root/$nomuser.zone
+		if sudo grep -q  @$domaine:$adresse /etc/tinydns/root/$nomuser.zone; then
+			sudo sed -i /"@$domaine:$adresse"/d /etc/tinydns/root/$nomuser.zone
 			sudo sed -i '10 s/'\ $domaine'//g' /etc/postfix/main.cf
 			sudo rm -r /var/mail/$domaine
 			sudo rm /var/www/rainloop/data/_data_/_default_/domains/$domaine.ini
@@ -23,8 +23,8 @@ elif [ $enrg = "mail" -a "$domaine" != "aos.itinet.fr" ]; then
 			sudo echo "Ce mail est inconnu!"
 		fi
 	else
-		if sudo grep -q  @$nomuser.$domaine:$adresse /etc/tinydns/root/$nomuser.zone; then
-			sudo sed -i /"@$nomuser.$domaine:$adresse"/d /etc/tinydns/root/$nomuser.zone
+		if sudo grep -q  @$domaine:$adresse /etc/tinydns/root/$nomuser.zone; then
+			sudo sed -i /"@$domaine:$adresse"/d /etc/tinydns/root/$nomuser.zone
 		else 
 			sudo echo "Ce mail est inconnu!"
 		fi
