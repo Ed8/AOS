6c427d46351b185420ac095d53fd69d2eeae6b8a
DimtriTchapmi <tchapmiwagna@intechinfo.fr>
2015-12-04 04:55:52 -0800

    Modification script dns

--- script/addfilezone.sh
+++ script/addfilezone.sh
@@ -8,7 +8,7 @@ if  [ "$domaine" != "aos.itinet.fr" ]; then
       		sudo echo "Le domaine existe deja!" 
     	else 
        		sudo touch /etc/tinydns/root/$nomuser.zone
-       		sudo echo ".$domaine:$adresse:259200 " >> /etc/tinydns/root/$nomuser.zone
+       		sudo echo ".$domaine:$adresse:NS:259200 " >> /etc/tinydns/root/$nomuser.zone
     	fi
 fi
 sudo bash updatezone.sh
